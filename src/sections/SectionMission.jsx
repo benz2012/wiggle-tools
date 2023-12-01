0a698a24187ad243a8e254842ad18bdfdf627c66
Ben Zenker <imbenzenker@gmail.com>
2023-11-30 22:36:00 -0700

    finalizes mission statement

--- src/sections/SectionMission.jsx
+++ src/sections/SectionMission.jsx
@@ -23,23 +23,40 @@ const SectionMission = () => (
     </Heading2>
 
     <Body style={{ textAlign: 'center' }}>
-      Wouldn't it be great if you had access to basic creativity software without paying awful subscriptions fees to annoying corporate giants? Are you fed up with companies that have for too long taken advantage of their customers, and lead to the Enshittification of the Internet?! Have you ever wondered why thousands of incredible software projects exist for free, creating a software backbone of our computers and our internet, but "I can't export this damn video without a watermark"?
-      <br /><br />
-      Web technologies have advanced greatly over the past few decades to the point where it's very easy to build creative apps directly in the web. You've likely seen many of these before, but unfortunatley big software applications require lots of engineers to build, and so we're stuck with more software that requires payment to unlock it's full potential. To do otherwise would threaten the livelihoods of the engineers that build them -- and that's not what this mission is about.
-      <br /><br />
-      The Wiggle mission is about taking the one thing we can offer this world, our time, and investing it into small & useful tools that accomplish dedicated tasks in the creative world. Many developers have done this before, but the road to meaningful and intuitve creative apps is not always easy. We've received awesome tools like Gimp and VLC, but are left to cope with UI clunkiness, feature bloat, and occasionally spicy forum arguments about ideological opinons on how software should be built.
-      <br /><br />
-      And so, instead of being fed-up with the state of the internet, software, and capitalism-driven models, I've decided to do something about it. This is why Wiggle Tools will be Open-Source and Free Forever, with No Ads. I'm sick of those things myself, so why would I create something I'm passionite about just to push the same on my users. While everyone else is busy trying to make money, I'll be busy trying to make great creative tools on the web.
-      <br /><br />
-      ------
-      <br /><br />
-      Why me?<br />
-      Blah blah blahhhh.
-      {/* <br /><br /> */}
+      The Wiggle Mission is to create powerful creative tools that are easy to access,
+      enjoyable to interact with, and free to use, forever. Subscription Fees, Advertisements,
+      large package downloads, and clunky user experiences: We're leaving those in the dust.<br />
+      Small & useful tools that accomplish dedicated tasks, for the creative world,
+      deserve to exist this way. It's that simple.
     </Body>
-    <Body style={{ textAlign: 'center' }}>
-    Why now?<br />
-      Blah blah blahhhh.
+
+    <Body style={{ textAlign: 'center', width: '20%' }}>
+      <hr />
+    </Body>
+
+    <Body style={{ textAlign: 'center', width: '84%' }}>
+      Why me?<br />
+      I love digital graphics.
+      I spent at least a decade producing/editing videos.
+      I have a unique degree that mixes TV/Film, Technology, and Imaging Science.
+      I worked in Visual Effects developing plugins & scripts for a handful of media industry tools.
+      I get excited reading about video codecs. I'm genuinely passionate about building great creative tools.
+    </Body>
+
+    <Body style={{ textAlign: 'center', width: '80%' }}>
+      Why now?<br />
+      Because in 2016 I lost my college discount on certain creative tools and was compelled to make simplistic & free versions of them.
+      Because even after many years of dabbling in this project, others like Photopea give me hope that people would value similar tools.
+      Because web technologies have advanced greatly over the past decade.
+    </Body>
+
+    <Body style={{ textAlign: 'center', width: '20%' }}>
+      <hr />
+    </Body>
+
+    <Body style={{ textAlign: 'center', width: '80%' }}>
+      It's unfortunate that generative AI has arrived in full-force, and likely makes this product obsolete in many ways.
+      But for those who still value direct control over their creative output, we've got the tools for you.
     </Body>
   </CenterWideSection>
 )
