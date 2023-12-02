cfdcf348a23c9ecda487f74e672929b5b0827f49
Ben Zenker <imbenzenker@gmail.com>
2023-12-02 12:20:25 -0700

    adds appcheck for protection

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,11 @@
 import '@fontsource/roboto'
 import '@fontsource/roboto-slab/700.css'
 import { initializeApp } from 'firebase/app'
+import { initializeAppCheck, ReCaptchaEnterpriseProvider } from 'firebase/app-check'
 
 export const onClientEntry = () => {
   // Initialize Firebase
-  initializeApp({
+  const app = initializeApp({
     apiKey: "AIzaSyBNBuGnLIcqDwPz9zcT_ih101xWkAY7MxA",
     authDomain: "wiggle-tools.firebaseapp.com",
     databaseURL: "https://wiggle-tools-default-rtdb.firebaseio.com",
@@ -13,6 +14,16 @@ export const onClientEntry = () => {
     messagingSenderId: "964928791153",
     appId: "1:964928791153:web:4abc0209e2bde0d262f1d0"
   })
+
+  // Create a ReCaptchaEnterpriseProvider instance using reCAPTCHA Enterprise
+  if (process.env.NODE_ENV !== 'development') {
+    initializeAppCheck(app, {
+      provider: new ReCaptchaEnterpriseProvider('6LeO2CEpAAAAACVx92joPBbU8Mt-NSTpUNn3fEYj'),
+      isTokenAutoRefreshEnabled: true
+    })
+  } else {
+    console.log('Skipping ReCaptchaEnterprise AppCheck')
+  }
 }
 
 export const onInitialClientRender = () => {
