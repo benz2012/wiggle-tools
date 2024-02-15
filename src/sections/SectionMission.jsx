daffda4a16a0095a4cb8167b46dc08ebf5aa7013
Ben Zenker <imbenzenker@gmail.com>
2024-02-15 13:39:46 -0700

    updates mission statement

--- src/sections/SectionMission.jsx
+++ src/sections/SectionMission.jsx
@@ -42,30 +42,16 @@ const SectionMission = () => (
 
     <Body style={{ textAlign: 'center' }}>
       The Wiggle Mission is to create powerful creative tools that are easy to access,
-      enjoyable to interact with, and free to use, forever. Subscription Fees, Advertisements,
-      large package downloads, and clunky user experiences: We're leaving those in the dust.<br />
+      enjoyable to interact with, and free to use, forever. Subscription fees, aggressive advertisements, exports with watermarks,
+      large package downloads, and clunky user experiences: We're leaving those behind.
       Small & useful tools that accomplish dedicated tasks, for the creative world,
-      deserve to exist this way. It's that simple.
+      deserve to exist this way.
     </Body>
 
-    <RuleContainer>
-      <StyledRule />
-    </RuleContainer>
-
-    <Body style={{ textAlign: 'center', width: '84%' }}>
-      Why me?<br />
-      I love digital graphics.
-      I spent at least a decade producing/editing videos.
-      I have a unique degree that mixes TV/Film, Technology, and Imaging Science.
-      I worked in Visual Effects developing plugins & scripts for a handful of media industry tools.
-      I get excited reading about video codecs. I'm genuinely passionate about building great creative tools.
-    </Body>
-
-    <Body style={{ textAlign: 'center', width: '80%' }}>
-      Why now?<br />
-      Because in 2016 I lost my college discount on certain creative tools and was compelled to make simplistic & free versions of them.
-      Because even after many years of dabbling in this project, others like Photopea give me hope that people would value similar tools.
-      Because web technologies have advanced greatly over the past decade.
+    <Body style={{ textAlign: 'center' }}>
+      So many creative tools on the market operate like a giant toolbox, shoving too many features in your face.
+      This creates a steep learning curve, sluggish startup times, and general frustration that gets in your way when trying to create a simple visual.
+      The tools we build aim to equip you with a core feature, and nothing else. We hope to become your favorite screwdriver within that giant toolbox.
     </Body>
 
     <RuleContainer>
@@ -73,7 +59,7 @@ const SectionMission = () => (
     </RuleContainer>
 
     <Body style={{ textAlign: 'center', width: '80%' }}>
-      It's unfortunate that generative AI has arrived in full-force, and likely makes this product obsolete in many ways.
+      We acknowledge that generative AI has arrived in full force, and likely makes these products obsolete.
       But for those who still value direct control over their creative output, we've got the tools for you.
     </Body>
   </CenterWideSection>
